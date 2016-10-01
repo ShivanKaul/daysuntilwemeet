8f52fb8b85d33fa9c74394c3f8249d7b56eb8f27
ShivanKaul <shivankaul.1993@gmail.com>
2016-10-01 16:52:33 -0400

    Date calculation the right way

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 window.onload = function() {
     // Calculate days left
-    let targetDate = new Date(2016, 9, 29)
-    let today = new Date()
-    let daysLeft = targetDate.getDate() - today.getDate() - 1 // don't count today
+    // Get UNIX timestamp, convert to days since epoch
+    let targetDate = Math.floor((new Date(2016, 9, 29)).getTime() / 86400000)
+    let today = Math.floor((new Date()).getTime() / 86400000)
+    let daysLeft = targetDate - today - 1 // don't count today
     
     // set title of document
     document.title = daysLeft + ' days left!'
