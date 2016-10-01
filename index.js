7f8fd9d456d4d37d0f1c5fd0f3a9b259769df60f
ShivanKaul <shivankaul.1993@gmail.com>
2016-10-01 17:18:54 -0400

    Fun

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-window.onload = function() {
+function generateMessage(daysLeft) {
+    let message = "Only " + daysLeft + " days left!"
+
+    if (daysLeft % 3 == 0) {
+        message = "I'll see your face in " + daysLeft + " days!"
+    }
+    if (daysLeft % 4 == 0) {
+        message = "PupperTime in " + daysLeft + " days!"
+    }
+    if (daysLeft % 5 == 0) {
+        message = "I get to annoy you in " + daysLeft + " days <3!"
+    }
+    if (daysLeft < 10) {
+        message = "Soooo closeee only " + daysLeft + " days lefttt!"
+    }
+    if (daysLeft < 5) {
+        message = "Time to paaaack, only " + daysLeft + " days lefttt!"
+    }
+    if (daysLeft == 0) {
+        message = "OMG HI."
+    }
+    else if (daysLeft < 0) {
+        message = "I miss you."
+    }
+    
+    return message
+}
+
+
+window.onload = function () {
     // Calculate days left
     // Get UNIX timestamp, convert to days since epoch
     let targetDate = Math.floor((new Date(2016, 9, 29)).getTime() / 86400000)
     let today = Math.floor((new Date()).getTime() / 86400000)
     let daysLeft = targetDate - today - 1 // don't count today
-    
+
+    let message = generateMessage(daysLeft)
+
     // set title of document
     document.title = daysLeft + ' days left!'
     // set inner html ugh
-    document.getElementById("daysLeft").innerHTML = daysLeft;
+    document.getElementById("days").innerHTML = message
 }
