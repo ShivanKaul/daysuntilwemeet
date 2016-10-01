d6f56791f0e069aa5122bec58b3be8de8d2dbda5
ShivanKaul <shivankaul.1993@gmail.com>
2016-10-01 16:43:31 -0400

    Get days left and set innerhtml

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
-   document.getElementById("daysLeft").innerHTML= 27;
+    // Calculate days left
+    let targetDate = new Date(2016, 9, 29)
+    let today = new Date()
+    let daysLeft = targetDate.getDate() - today.getDate() - 1 // don't count today
+    
+    // set title of document
+    document.title = daysLeft + ' days left!'
+    // set inner html ugh
+    document.getElementById("daysLeft").innerHTML = daysLeft;
 }
