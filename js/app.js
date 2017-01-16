e87bd0bbe6a9dbf5cf73a4646613007351c1758b
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2017-01-16 11:24:49 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ import moment from 'moment'
 
 moment().format()
 
+var targetDate = moment([2016, 9, 29]) // 0 -> Jan
+var heartLink = "https://giphy.com/gifs/excited-corgi-xT77XKxcPqxIZqUrwk"
+
 // Top level
 var Page = React.createClass({
   getDefaultProps: function () {
@@ -45,7 +48,7 @@ var Days = React.createClass({
       textAlign: 'center',
       fontFamily: 'Lato',
     }
-    var targetDate = moment([2016, 9, 29]) // 0 -> Jan
+    // Target date defined above
     var dateToday = moment()
     var daysLeft = targetDate.diff(dateToday, 'days')
 
@@ -73,7 +76,7 @@ var Heart = React.createClass({
     return (
       <div style={this.props.style}>
         <p style={paragraph}>
-          <a target="_blank" href="https://giphy.com/gifs/excited-corgi-xT77XKxcPqxIZqUrwk"> <img style={heart} title='yay' alt='this was supposed to be a heart' src={'./heart.png'} /> </a>
+          <a target="_blank" href={heartLink}> <img style={heart} title='yay' alt='this was supposed to be a heart' src={'./heart.png'} /> </a>
         </p>
       </div>
     )
